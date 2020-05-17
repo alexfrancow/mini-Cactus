d810a081fb9ad4aae7c1b9ae9f15078ade60dcf6
Alejandro Franco Vázquez <alejandrofrancovazquez@gmail.com>
2020-05-17 19:28:53 +0200

    Update serial.py

--- serial.py
+++ serial.py
@@ -1,5 +1,6 @@
 import serial
 import serial.tools.list_ports
+import time
 
 def list_com():
     arduino_coms = []
@@ -10,19 +11,25 @@ def list_com():
     print("Arduino ports:", arduino_coms)
     return arduino_coms
 
+
 ser = serial.Serial()
 ser.baudrate = 9600
 ser.port = 'COM4'
+
 ser.open()
-ser.write(b'MOYOXXL 2.0')
+time.sleep(2)
+#ser.write(b'MOYOXXL 2.0')
+ser.write(b'_ONOWiFi')
 
 mac_addreses = []
 ip_addreses = []
 while True:
      cc=str(ser.readline())
+     print(cc[2:][:-3])
      if "MAC address=" in cc:
-         mac_addreses.append(cc.split("=")[1])
-         print("MACCCCS", mac_addreses)
-     print(cc[2:][:-5])
+         mac_addreses.append(cc.split("=")[1].replace(" ", "").replace("\\n'", ""))
+         print("MACCCCS: ", mac_addreses)
+     if ".255" in cc:
+         break
      
 ser.close()
